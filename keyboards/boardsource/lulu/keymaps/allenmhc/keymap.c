0203a34e290ee24b7ac3dfbfc6188fc03c22ed74
Allen Cheung <me@allenc.com>
2024-12-01 01:36:16 -0800

    Fix up ⌃/Caps and ⌅/␣

--- keyboards/boardsource/lulu/keymaps/allenmhc/keymap.c
+++ keyboards/boardsource/lulu/keymaps/allenmhc/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   -   |    |   =   |------+------+------+------+------+------|
  * | Left |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | Right|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | RAISE|  Up  | Shift | /BackSP /       \Space \  | Enter| Down | LOWER|
+ *                   | RAISE|  Up  | Shift | /BackSP /       \Enter \  | Space| Down | LOWER|
  *                   |      |      |       |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -51,9 +51,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   QK_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-  KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOTE,
+  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOTE,
   KC_LEFT,  CKC_Z,  CKC_X,   CKC_C,   CKC_V,   KC_B, KC_MINS,   KC_EQL,  KC_N,    CKC_M,   CKC_COMM,CKC_DOT, CKC_SLSH,KC_RIGHT,
-                             RAISE,   KC_UP,OSM_SFT, KC_BSPC,   KC_SPC,  KC_ENT,  KC_DOWN, LOWER
+                             RAISE,   KC_UP,OSM_SFT, KC_BSPC,   KC_ENT,  KC_SPC,  KC_DOWN, LOWER
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -163,13 +163,13 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 // https://github.com/stasmarkin/sm_td/
 void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
     switch (keycode) {
-        SMTD_MT(CKC_Z, KC_Z, KC_LEFT_CTRL)
+        SMTD_MT(CKC_Z, KC_Z, KC_CAPS)
         SMTD_MT(CKC_X, KC_X, KC_LEFT_ALT)
         SMTD_MT(CKC_C, KC_C, KC_LEFT_GUI)
         SMTD_MT(CKC_V, KC_V, KC_LSFT)
         SMTD_MT(CKC_M, KC_M, KC_RSFT)
         SMTD_MT(CKC_COMM, KC_COMM, KC_RIGHT_GUI)
         SMTD_MT(CKC_DOT, KC_DOT, KC_RIGHT_ALT)
-        SMTD_MT(CKC_SLSH, KC_SLSH, KC_LEFT_CTRL)
+        SMTD_MT(CKC_SLSH, KC_SLSH, KC_CAPS)
     }
 }
